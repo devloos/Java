3a384f3d203bb4044f8ba2af999c779ea7557f0d
Carlos Aguilera <18aguilerac@gmail.com>
2022-09-29 18:49:12 -0700

    doing stuff

--- test/Main.java
+++ test/Main.java
@@ -1,54 +1,13 @@
-// import java.util.Scanner;
+import java.util.*;
 
-// class Exercise03_11 {
-//     public static void main(String[] args) {
-//         Scanner cin = new Scanner(System.in);
-        
-//         System.out.print("Enter a month in the year (e.g., 1 for Jan): ");
-//         int Month = cin.nextInt()-1;
-//         System.out.print("Enter a year: ");
-//         int Year = cin.nextInt();
-        
-//         String[] Months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-//         Integer[] Days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-//         boolean isLeapYearFormulaOne = (Year % 4 == 0 && Year % 100 != 0);
-//         boolean isLeapYearFormulaTwo = (Year % 4 == 0 && Year % 100 == 0 && Year % 400 == 0);
-
-//         if (isLeapYearFormulaOne || isLeapYearFormulaTwo) {
-//             System.out.println(Months[Month] + " " + Year + " has 29 days");
-//             return;
-//         }
-        
-//         System.out.println(Months[Month] + " " + Year + " has " + Days[Month] + " days");
-//     }
-// }
-
-import java.util.Scanner;
-
-class Exercise03_21 {
+class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int longest = 0;
-        int count = 0;
-        
-        int len;
-
-        while (input.hasNext()) {
-            len = input.next().length();
-            if (len == longest) {
-                ++count;
-            } else {
-                count = 1;
-            }
-            longest = (len > longest) ? len : longest;
-        }
+        int[][] array = {
+            {2, 4, 53},
+            {2, 4, 53},
+            {2, 4, 53}
+        };
+        for (int row = 0; row < array.length; row++)
+            System.out.println(Arrays.toString(array[row]));
     }
-    //public int Format(int & ) {
-        
-  //      res = (res == 1) ? 13 : res;
- //       res = (res == 2) ? 14 : res;
-   //     return res;
-   // }
 }
