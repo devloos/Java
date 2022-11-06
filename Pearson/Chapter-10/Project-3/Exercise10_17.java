d708a021399f21fd113b5ebf99289c9376c37a0f
Carlos Aguilera <18aguilerac@gmail.com>
2022-11-05 17:22:50 -0700

    removed empty main from commits

--- Pearson/Chapter-10/Project-3/Exercise10_17.java
+++ Pearson/Chapter-10/Project-3/Exercise10_17.java
@@ -5,6 +5,7 @@ public class Exercise10_17 {
     long n = (long)Math.sqrt(Long.MAX_VALUE) + 1;
 
     System.out.println("The ten numbers are");
+    System.out.println(n);
     for (int i = 0; i < 10; i++) {
       BigInteger t = BigInteger.valueOf(n++);
       System.out.println(t.multiply(t));
