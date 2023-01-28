14538e4a069590d4036bf86cc5011d9c18603820
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-27 22:07:09 -0800

    added

--- Projects/JavaFx/src/Main.java
+++ Projects/JavaFx/src/Main.java
@@ -1,6 +1,8 @@
 package src;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,6 +10,10 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     primaryStage.setTitle("My First JavaFX App");
+
+    Label label = new Label("THIS IS A LABEL");
+    Scene scene = new Scene(label, 640, 480);
+    primaryStage.setScene(scene);
 
     primaryStage.show();
   }
