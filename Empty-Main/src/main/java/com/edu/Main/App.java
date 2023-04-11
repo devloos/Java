c2eb2598b3fe5e180fdc5868adf1709bde8e8e8e
Carlos Aguilera <18aguilerac@gmail.com>
2023-04-10 17:33:11 -0700

    update

--- Empty-Main/src/main/java/com/edu/Main/App.java
+++ Empty-Main/src/main/java/com/edu/Main/App.java
@@ -5,7 +5,7 @@ import com.edu.Main.server.Server;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Client.main(args);
-        // Server.main(args);
+        // Client.main(args);
+        Server.main(args);
     }
 }
